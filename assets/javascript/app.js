3b0a6709f93ebc77e00c407a02a3513752b65d2e
gitMarcldo <marcldo@users.noreply.github.com>
2019-07-21 16:03:25 -0400

    added next train caluclations and appended to tbl

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,14 +26,63 @@ document.getElementById("submitTrain").addEventListener("click", (event) => {
         freq: document.querySelector("#freq").value.trim()
     };
 
+    nextTrainTime(train);
     database.ref("/trains").push(train);
 
-    console.log(train.trainName, train.destination, train.firstTrain, train.freq);
+    console.log(train.trainName, train.destination, train.firstTrain, train.freq, train.minAway, train.nextArrival);
 });
 
 database.ref("/trains").on("child_added", (snapshot) => {
-    console.log(snapshot.val());
+    let trains = snapshot.val();
+
+    let trainsHTML = `
+    <tr>
+        <td>${trains.trainName}</td>
+        <td>${trains.destination}</td>
+        <td>${trains.freq}</td>
+        <td>${trains.nextArrival}</td>
+        <td>${trains.minAway} mins away</td>
+    </tr>
+    `
+
+    //insert html as string to table body
+    document.getElementById("trainTableBody").insertAdjacentHTML("beforeEnd", trainsHTML);
+
 });
 
-//added comment
+// Solved Mathematically
+// Test case 2:
+// 16 - 00 = 16
+// 16 % 7 = 2 (Modulus is the remainder)
+// 7 - 2 = 5 minutes away
+// 5 + 3:16 = 3:21
+
+function nextTrainTime(train) {
+
+    let firstTrainConverted = moment(train.firstTrain, "HH:mm").subtract(1, "years");
+    console.log(firstTrainConverted);
+
+    let currentTime = moment();
+
+    let diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+    console.log(diffTime)
+
+    let tRemainder = diffTime % train.freq;
+
+    let tMinutesTillTrain = train.freq - tRemainder;
+
+    let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+    console.log(tMinutesTillTrain, moment(nextTrain).format("hh:mm"));
+
+    train.minAway = tMinutesTillTrain;
+    train.nextArrival = moment(nextTrain).format("hh:mm");
+
+}
+
+
+
+
+
+
 
